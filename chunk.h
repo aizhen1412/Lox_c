6e814a24af3acf7d2ce7622bf72387d73035ca4e
aizhen1412 <2662923241@qq.com>
2024-02-05 21:37:17 +0800

    complete the scanner

---
+++ chunk.h
@@ -0,0 +1,29 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+#include "value.h"
+
+typedef enum
+{
+    OP_CONSTANT,
+    OP_ADD,
+    OP_SUBTRACT,
+    OP_MULTIPLY,
+    OP_DIVIDE,
+    OP_NEGATE,
+    OP_RETURN,
+} OpCode;
+typedef struct
+{
+    int count;    // number of elements in the array
+    int capacity; // number of elements the array can hold before it needs to grow
+    uint8_t *code;
+    int *lines;
+    ValueArray constants;
+} Chunk;
+void initChunk(Chunk *chunk);
+void freeChunk(Chunk *chunk);
+void writeChunk(Chunk *chunk, uint8_t byte, int line);
+int addConstant(Chunk *chunk, Value value);
+#endif
